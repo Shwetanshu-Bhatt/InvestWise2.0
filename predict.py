c8596264038f16f10599e1c792da82801e60194e
Shwetanshu-Bhatt <shwetanshubhatt@gmail.com>
2024-11-09 23:19:37 +0530

    message for predict

--- predict.py
+++ predict.py
@@ -1,15 +1,28 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import pandas as pd
 import joblib
 
-
-# In[2]:
-
+def get_user_data():
+    open_prices = []
+    close_prices = []
+    
+    days = int(input("Enter the number of days (at least 5): "))
+    
+    if days < 5:
+        print("Please enter data for at least 5 days.")
+        return get_user_data()
+    
+    for i in range(days):
+        open_price = float(input(f"Enter the Open price for day {i+1}: "))
+        close_price = float(input(f"Enter the Close price for day {i+1}: "))
+        open_prices.append(open_price)
+        close_prices.append(close_price)
+    
+    new_data = pd.DataFrame({
+        'Open': open_prices[::-1],  
+        'Close': close_prices[::-1]   
+    })
+    
+    return new_data
 
 def prepare_data(df):
     df['Prev_Open'] = df['Open'].shift(1)
@@ -19,24 +32,11 @@ def prepare_data(df):
     y = df[['Open', 'Close']]
     return X, y
 
-
-# In[3]:
-
-
 general_model = joblib.load('general_linear_model.pkl')
 
-new_data = pd.DataFrame({
-    'Open': [2960.00, 2967.90, 2864.00, 2803.00, 2701.10][::-1],  
-    'Close': [2949.50, 2947.25, 2969.30, 2848.60, 2798.65][::-1]   
-})
+new_data = get_user_data()
+
 new_X, _ = prepare_data(new_data)
 
 predictions = general_model.predict(new_X)
-print(predictions[-1]) 
-
-
-# In[ ]:
-
-
-
-
+print(predictions[-1])
